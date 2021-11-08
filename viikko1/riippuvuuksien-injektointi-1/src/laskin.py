9c8cba3ddc73a670c39462994c7c1ba1a2461fe8
Ari Kauppi <ari.kauppi@santelo.fi>
2021-11-08 23:29:35 +0200

    Tehtävä 14 tehty

--- viikko1/riippuvuuksien-injektointi-1/src/laskin.py
+++ viikko1/riippuvuuksien-injektointi-1/src/laskin.py
@@ -1,3 +1,5 @@
+from konsoli_io import KonsoliIO
+
 class Laskin:
     def __init__(self, io):
         self._io = io
@@ -20,3 +22,14 @@ class Laskin:
 
     def _laske_summa(self, luku1, luku2):
         return luku1 + luku2
+
+def main():
+    io = KonsoliIO()
+    laskin = Laskin(io)
+
+    laskin.suorita()
+
+# Pelkkä main()-metodi tuntui aiheuttavan virheen.
+# Kelpaakohan tämä kuitenkin ratkaisusta?
+if __name__ ==  "__main__":
+    main()
