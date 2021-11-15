c0214b4ddf6280588a6875f6d615942d5648654a
Ari Kauppi <ari.kauppi@santelo.fi>
2021-11-15 22:32:42 +0200

    Tilavuuden määrittely yksinkertaistettu

--- viikko1/varasto/src/varasto.py
+++ viikko1/varasto/src/varasto.py
@@ -1,36 +1,6 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            
-            if tilavuus > alku_saldo:
-                print("Täällä hieman kompleksisuutta lisäillään")
-            self.tilavuus = tilavuus
-
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
-
-        if alku_saldo < 0.0:
-            # virheellinen, nollataan
-            self.saldo = 0.0
-        elif alku_saldo <= tilavuus:
-            # mahtuu
-            self.saldo = alku_saldo
-        else:
-            # täyteen ja ylimäärä hukkaan!
-            self.saldo = tilavuus
-        
-        # Uudelleen yläpuolelta jotta hajoaa
-        if tilavuus > 0.0:
-                
-            if tilavuus > alku_saldo:
-                print("Täällä hieman kompleksisuutta lisäillään")
-            self.tilavuus = tilavuus
-
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
-
+        self.tilavuus = 0.0 if tilavuus <= 0.0 else tilavuus
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
@@ -54,20 +24,13 @@ class Varasto:
             self.saldo = self.tilavuus
 
     def ota_varastosta(self, maara):
-        """
-        0123456789101112131415161718192021222324252627282930313233343536373839404142434445464748495051525354555657585960616263646566676869707172737475767778798081828384858687888990919293949596979899100101102103104105106107108109
-        """
-
         if maara < 0:
             return 0.0
         if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
             return kaikki_mita_voidaan
-        if maara < self.saldo:
-            print("Rikkoutuu")
         self.saldo = self.saldo - maara
-
         return maara
 
     def __str__(self):
