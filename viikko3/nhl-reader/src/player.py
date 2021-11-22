335a8f8208e5a9cac2dc83eebcddaf3f23d20483
Ari Kauppi <ari.kauppi@santelo.fi>
2021-11-22 18:47:55 +0200

    viikko3, 1. tehtävä tehty

--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,15 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, team, goals=0, assists=0):
         self.name = name
+        self.team = team
+        self.goals = goals
+        self.assists = assists
+
     
     def __str__(self):
-        return self.name
+        return "{} team {} goals {} assists {}".format(
+            self.name,
+            self.team,
+            self.goals,
+            self.assists
+        )
