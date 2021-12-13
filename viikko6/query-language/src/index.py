8b050b68a496e21229f59b30e5a4f876c6de5d39
Ari Kauppi <ari.kauppi@santelo.fi>
2021-12-13 05:04:58 +0200

    Tehtävän 3 Or-luokka toteutettu

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,16 +1,27 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, HasFewerThan, PlaysIn, Not
+from matchers import And, HasAtLeast, HasFewerThan, PlaysIn, Not, Or
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
+    # Vähintään 30 maalia tai 50 syöttöä
+    # matcher = Or(
+    #     HasAtLeast(30, "goals"),
+    #     HasAtLeast(50, "assists")
+    # )
+
     matcher = And(
-        HasFewerThan(1, "goals"),
-        PlaysIn("NYR")
+        HasAtLeast(40, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("NYI"),
+            PlaysIn("BOS")
+        )
     )
+
 
     for player in stats.matches(matcher):
         print(player)
